e369aa311c01cd2f9ff5152bb576185525940fdc
ankush-tester65 <ankush.ukey@xecurify.com>
2026-03-24 15:00:57 +0530

    adding sso module

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MiniOrange_MagentoSecuritySuite',
+    __DIR__
+);
+
